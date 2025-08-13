0277d5ea997cb4af69ecc5b03aeb1c1ea4eacba5
the-db-architect <the-db-architect@users.noreply.github.com>
2025-08-12 20:44:20 -0400

    chore: New auth endpoint

--- src/internal_vars.rs
+++ src/internal_vars.rs
@@ -5,7 +5,7 @@ pub static IPFS_HOST: LazyLock<String> = LazyLock::new(|| {
 });
 
 pub static AUTH_HOST: LazyLock<String> = LazyLock::new(|| {
-    std::env::var("AUTH_API").unwrap_or_else(|_| "https://load-auth-vskt.shuttle.app".to_string())
+    std::env::var("AUTH_API").unwrap_or_else(|_| "https://k8s.load-auth-service.load.network".to_string())
 });
 
 pub static S3_ENDPOINT: LazyLock<String> = LazyLock::new(|| {
